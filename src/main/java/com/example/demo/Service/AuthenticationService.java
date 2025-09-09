8c821318430d9171edc4f3f113eff8c55f36244d
DonaldKisaka <kisakadonald1@gmail.com>
2025-09-09 12:34:07 +0300

    AuthenticationService

--- src/main/java/com/example/demo/Service/AuthenticationService.java
+++ src/main/java/com/example/demo/Service/AuthenticationService.java
@@ -1,4 +1,127 @@
 package com.example.demo.Service;
 
+import com.example.demo.dto.LoginUser;
+import com.example.demo.dto.RegisterUser;
+import com.example.demo.dto.VerifyUser;
+import com.example.demo.model.User;
+import com.example.demo.model.UserRepository;
+import jakarta.mail.MessagingException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
+
+@Service
 public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final EmailService emailService;
+
+    public AuthenticationService(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            AuthenticationManager authenticationManager,
+            EmailService emailService
+    ) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.emailService = emailService;
+    }
+
+    public User SignUp(RegisterUser input) {
+        User user = new User(input.getUsername(), passwordEncoder.encode(input.getPassword()), input.getEmail());
+        user.setVerificationCode(generateVerificationCode());
+        user.setVerificationCodeExpiresAt(LocalDateTime.now().plusMinutes(15));
+        user.setEnabled(false);
+        sendVerificationEmail(user);
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginUser input) {
+        User user = userRepository.findByEmail(input.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!user.isEnabled()) {
+            throw new  RuntimeException("Account not verified: Please verify your account");
+        }
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+
+        );
+        return user;
+    }
+
+    public void verifyUser(VerifyUser input) {
+       Optional<User>  optionalUser = userRepository.findByEmail(input.getEmail());
+       if (optionalUser.isPresent()) {
+           User user = optionalUser.get();
+           if (user.getVerificationCodeExpiresAt().isBefore(LocalDateTime.now())) {
+               throw new RuntimeException("Verification code has expired");
+           }
+           if (user.getVerificationCode().equals(input.getVerificationCode())) {
+               user.setEnabled(true);
+               user.setVerificationCode(null);
+               user.setVerificationCodeExpiresAt(null);
+               userRepository.save(user);
+           } else {
+               throw new RuntimeException("Invalid verification code");
+           }
+       } else {
+           throw new RuntimeException("User not found");
+       }
+    }
+
+    public void resendVerificationCode(String email) {
+        Optional<User>  optionalUser = userRepository.findByEmail(email);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            if (user.isEnabled()) {
+                throw new RuntimeException("Account is already verified");
+            }
+            user.setVerificationCode(generateVerificationCode());
+            user.setVerificationCodeExpiresAt(LocalDateTime.now().plusMinutes(15));
+            sendVerificationEmail(user);
+            userRepository.save(user);
+        } else {
+            throw new RuntimeException("User not found");
+        }
+    }
+
+    public void sendVerificationEmail(User user) {
+        String subject = "Account verification";
+        String verificationCode = user.getVerificationCode();
+        String htmlMessage = "<html>"
+                + "<body style=\"font-family: Arial, sans-serif;\">"
+                + "<div style=\"background-color: #f5f5f5; padding: 20px;\">"
+                + "<h2 style=\"color: #333;\">Welcome to our app!</h2>"
+                + "<p style=\"font-size: 16px;\">Please enter the verification code below to continue:</p>"
+                + "<div style=\"background-color: #fff; padding: 20px; border-radius: 5px; box-shadow: 0 0 10px rgba(0,0,0,0.1);\">"
+                + "<h3 style=\"color: #333;\">Verification Code:</h3>"
+                + "<p style=\"font-size: 18px; font-weight: bold; color: #007bff;\">" + verificationCode + "</p>"
+                + "</div>"
+                + "</div>"
+                + "</body>"
+                + "</html>";
+        try {
+            emailService.sendVerificationEmail(user.getEmail(), subject, htmlMessage );
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String generateVerificationCode() {
+        Random random = new Random();
+        int code = random.nextInt(999999) + 111111;
+        return String.valueOf(code);
+    }
+
 }
